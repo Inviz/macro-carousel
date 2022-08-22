c9c5b5f7fb54baf8db93c1f877a9595a015ad327
Sitecore <sitecore@Yaroslaffs-MacBook-Pro.local>
2022-08-23 02:34:04 +0800

    Cleanup stuff

--- dist/macro-carousel.js
+++ dist/macro-carousel.js
@@ -66,7 +66,8 @@
   }
 
   /**
-   * Clamps a number according to its absolute value, but still retainig its sign.
+   * Clamps a number according to its absolute value, but still retaining its
+   *  sign.
    * @param {number} x The number to be clamped.
    * @param {number} [min] The min value.
    * @param {number} [max] The max value.
@@ -120,14 +121,14 @@
    * @param {number} value
    */
   function intSetter(element, attributeName, value) {
-    element.setAttribute(attributeName, value);
+    element.setAttribute(attributeName, `${value}`);
   }
 
   /**
    * Standard getter for a Custom Element int property reflected to attribute.
    * @param {HTMLElement} element
    * @param {string} attributeName
-   * @param {string} [defaultValue=0]
+   * @param {number} [defaultValue=0]
    * @return {number} Whether the element has that specific attribute
    */
   function intGetter(element, attributeName, defaultValue = 0) {
@@ -192,7 +193,7 @@
    * @param {string|number} propertyValue The property value.
    */
   function setCSSCustomProperty(element, propertyName, propertyValue) {
-    element.style.setProperty(propertyName, propertyValue);
+    element.style.setProperty(propertyName, `${propertyValue}`);
     if (window.ShadyCSS) {
       window.ShadyCSS.styleSubtree(element, {[propertyName]: propertyValue});
     }
@@ -345,7 +346,7 @@
 
 
   // A fraction of the slider width, a size used to compute
-  // by how many slides whould the slider move after a swipe.
+  // by how many slides the slider would move after a swipe.
   const _velocityThresholdFactor = 0.5;
 
   // How many slides more than slidesPerView can be swiped
@@ -354,7 +355,7 @@
 
   // How strictly the carousel detects a horizontal drag.
   // The angle (in degrees) should be in range (0, 90)
-  // 45 degress is the neutral angle.
+  // 45 degrees is the neutral angle.
   // The higher the value, the stricter the detection.
   const _dragAngleAllowance = Math.abs(roundedTan(35));
 
@@ -397,7 +398,13 @@
    * @typedef {object} SlideInfo
    * @property {HTMLElement} element The DOM element.
    * @property {number} layoutIndex The real index in the layout.
-   * @property {number} position The current position within the slder (in px).
+   * @property {number} position The current position within the slider (in px).
+   */
+
+  /**
+   * @typedef {object} TrackingPoint
+   * @property {Date} time Current time.
+   * @property {number} x Last position of the pointer on the x axis.
    */
 
   /**
@@ -411,7 +418,7 @@
     constructor() {
       /*
        * Runs anytime a new instance is created (in HTML or JS).
-       * The construtor is a good place to create shadow DOM, though you should
+       * The constructor is a good place to create shadow DOM, though you should
        * avoid touching any attributes or light DOM children as they may not
        * be available yet.
        */
@@ -637,15 +644,15 @@
       this._lastDraggedLayoutIndex = undefined;
 
       /**
-       * Array containining the translation value assumed by the slidesWrapper in
+       * Array containing the translation value assumed by the slidesWrapper in
        * the last 100ms. Used to compute the starting velocity when decelerating.
-       * @type {Array<number>}
+       * @type {Array<TrackingPoint>}
        * @private
        */
       this._trackingPoints = [];
 
       /**
-       * Flag used to limit the number of udpates to the slidesWrapper to once
+       * Flag used to limit the number of updates to the slidesWrapper to once
        * per frame (the pointer events may fire more frequently than that).
        * @type {boolean}
        * @private
@@ -700,7 +707,7 @@
 
     /**
      * Queries the Shadow DOM looking for an element matching the id
-     * @param {string} selector The id of the element
+     * @param {string} id The id of the element
      * @returns {HTMLElement}
      */
     _getShadowElementById(id) {
@@ -914,7 +921,7 @@
 
     /**
      * Computes the previous index.
-     * @param {number} i The index of reference used to compure the previous.
+     * @param {number} i The index of reference used to compute the previous.
      * @return {number} The previous index with respect to the input.
      * @private
      */
@@ -946,7 +953,7 @@
 
     /**
      * Computes the next index.
-     * @param {number} i The index of reference used to compure the next.
+     * @param {number} i The index of reference used to compute the next.
      * @return {number} The next index with respect to the input.
      * @private
      */
@@ -1358,7 +1365,7 @@ value. Add CSS units to its value to avoid breaking the slides layout.`);
 
 
     /**
-     * Extracts the slide's data index (i.e. between 0 and this._slides.lenght -1)
+     * Extracts the slide's data index (i.e. between 0 and this._slides.length -1)
      * from its layoutIndex.
      * @param {number} layoutIndex The slide's layoutIndex
      * @return {number} The slide's data index.
@@ -1464,7 +1471,7 @@ value. Add CSS units to its value to avoid breaking the slides layout.`);
         // Slides in view don't have the `inert` attribute and can be focused.
         if (isSlideInView) {
           slideEl.removeAttribute(ATTRS.STANDARD.INERT);
-          slideEl.setAttribute(ATTRS.STANDARD.TABINDEX, -1);
+          slideEl.setAttribute(ATTRS.STANDARD.TABINDEX, '-1');
         } else {
           slideEl.setAttribute(ATTRS.STANDARD.INERT, '');
         }
@@ -1641,7 +1648,7 @@ value. Add CSS units to its value to avoid breaking the slides layout.`);
       this._slides = this._getSlides();
       this._slides.forEach(slide => {
         if (!slide.element.hasAttribute(ATTRS.STANDARD.TABINDEX)) {
-          slide.element.setAttribute(ATTRS.STANDARD.TABINDEX, -1);
+          slide.element.setAttribute(ATTRS.STANDARD.TABINDEX, '-1');
         }
 
         if (this.getAttribute(ATTRS.STANDARD.ROLE) === ATTR_VALUES.ROLES.LIST) {
@@ -1659,7 +1666,7 @@ value. Add CSS units to its value to avoid breaking the slides layout.`);
       const shouldForceSelectedUpdate = this._slides.length > 0 &&
           this._lastViewIndex === -1;
 
-      // Calling internalUpdate instead of update, to avoid race coniditions
+      // Calling internalUpdate instead of update, to avoid race conditions
       // (update is debounced). This is because the number of slides is
       // essential for computing the remaining internal values.
       this._internalUpdate();
@@ -1810,7 +1817,7 @@ value. Add CSS units to its value to avoid breaking the slides layout.`);
 
     /**
      * Stores the last 100ms worth of tracking data from pointer events.
-     * @param {number} x The x coordinate value to strore
+     * @param {number} x The x coordinate value to store.
      * @private
      */
     _addTrackingPoint(x) {
@@ -1989,7 +1996,7 @@ value. Add CSS units to its value to avoid breaking the slides layout.`);
       const newPosition = this._wrapperTranslateX + this._decelVelocity;
 
       // Keep animating until the carousel is close to the snapping point
-      // with a very small veloity. This results in a springy effect.
+      // with a very small velocity. This results in a springy effect.
       // Do not animate if the reduced-motion mode is enabled.
       const tooFarOrTooFast = Math.abs(snapX - newPosition) >= 1 ||
           Math.abs(this._decelVelocity) >= 1;
